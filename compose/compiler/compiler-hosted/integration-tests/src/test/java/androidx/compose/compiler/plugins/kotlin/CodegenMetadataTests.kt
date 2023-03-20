683c6178e1c40a310d2f9d565a987327eab96254
Steven Schäfer <stevenschaefer@google.com>
2023-03-20 12:30:18 +0100

    Compose Tests: Support running tests with K2

--- compose/compiler/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/CodegenMetadataTests.kt
+++ compose/compiler/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/CodegenMetadataTests.kt
@@ -19,7 +19,7 @@ package androidx.compose.compiler.plugins.kotlin
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.junit.Test
 
-class CodegenMetadataTests : AbstractLoweringTests() {
+class CodegenMetadataTests(useFir: Boolean) : AbstractLoweringTests(useFir) {
     override fun CompilerConfiguration.updateConfiguration() {
         put(ComposeConfiguration.LIVE_LITERALS_ENABLED_KEY, true)
     }
